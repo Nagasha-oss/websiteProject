a3e3f1482cadbeab736f0f40fdadbde10693322d
Nagasha Christine <cnagasha@usiu.ac.ke>
2025-04-15 13:53:16 +0300

    HIIIII

---
+++ functions.py
@@ -0,0 +1,25 @@
+
+def greet(name):
+    return f"hello,{name}"
+print(greet("nagasha"))
+def add(a,b):
+    return(a+b)
+sum =add(2,5)
+print(sum)
+
+### checcking if the numbeer is even
+def is_even(number):
+    if number%2==0:
+        return True
+    return False
+print(is_even(5))
+## converting kilometres to metres
+def convert_to_Metres(kilometres):
+    return kilometres/1000
+print(convert_to_Metres(30000))
+##finding the maximum number
+def find_max(numbers):
+    return max(numbers)
+list[2,4,5,6,]
+print(find_max(list))
+
