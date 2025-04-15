cc203c5b03a53896e4c53bcea51d5a836f552c7b
Nagasha Christine <cnagasha@usiu.ac.ke>
2025-04-15 11:49:42 +0300

    fourth commit of files

---
+++ github.py
@@ -0,0 +1,19 @@
+'''
+a=10
+print("Type of a: " ,type(a))
+c='h'
+print("Type of c:",type (c))
+List = []
+print ("Initial blank list:",List)
+List = ['Geeks','For','Geeks']
+print("List with the use of string:",List)
+print("The type of keyword True", type(True))
+Set1=set("This is Nagasha")
+print(Set1)
+'''
+Dictionary1={1:'geeks',2:'yoo',3:'good'}
+Dictionary2={'name':'nagasha','age':45}
+print(Dictionary2.get('name'))
+x=2
+y=6
+print(x+y)
